b39e5e79844264cb3aedbe1d5ad6576fb51f7d89
peterdinis <petter1@pobox.sk>
2025-02-14 18:24:40 +0100

    FOO

--- app/_components/shared/SettingsDialog.tsx
+++ app/_components/shared/SettingsDialog.tsx
@@ -7,12 +7,10 @@ import { Button } from "@/components/ui/button"
 import {
     Form,
     FormControl,
-    FormDescription,
     FormField,
     FormItem,
     FormLabel,
 } from "@/components/ui/form"
-import { Switch } from "@/components/ui/switch"
 import { toast } from "@/hooks/use-toast";
 import { ModeToggle } from "./ModeToggle";
 
@@ -48,48 +46,6 @@ const SettingsDialog: FC = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
                         <div>
                             <div className="space-y-4">
-                                <FormField
-                                    control={form.control}
-                                    name="marketing_emails"
-                                    render={({ field }) => (
-                                        <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-                                            <div className="space-y-0.5">
-                                                <FormLabel>Marketing emails</FormLabel>
-                                                <FormDescription>
-                                                    Receive emails about new products, features, and more.
-                                                </FormDescription>
-                                            </div>
-                                            <FormControl>
-                                                <Switch
-                                                    checked={field.value}
-                                                    onCheckedChange={field.onChange}
-                                                />
-                                            </FormControl>
-                                        </FormItem>
-                                    )}
-                                />
-                                <FormField
-                                    control={form.control}
-                                    name="security_emails"
-                                    render={({ field }) => (
-                                        <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-                                            <div className="space-y-0.5">
-                                                <FormLabel>Security emails</FormLabel>
-                                                <FormDescription>
-                                                    Receive emails about your account security.
-                                                </FormDescription>
-                                            </div>
-                                            <FormControl>
-                                                <Switch
-                                                    checked={field.value}
-                                                    onCheckedChange={field.onChange}
-                                                    disabled
-                                                    aria-readonly
-                                                />
-                                            </FormControl>
-                                        </FormItem>
-                                    )}
-                                />
                                 <FormField
                                     control={form.control}
                                     name="security_emails"
