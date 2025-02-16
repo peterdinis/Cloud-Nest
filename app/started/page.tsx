d2372e799c8599eda699a2ca48eb1d2b14185fea
peterdinis <petter1@pobox.sk>
2025-02-16 12:54:13 +0100

    feat: New page folders

---
+++ app/started/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const StartedFilesPage: NextPage = () => {
+    return (
+        <>
+            Started Files
+        </>
+    )
+}
+
+export default StartedFilesPage
